0c8aecb7ce329d66023507fb7998fc7fbe5658e1
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-10 10:42:59 -0300

    Update Led.java

--- src/esoterum/type/Led.java
+++ src/esoterum/type/Led.java
@@ -1,1 +1,27 @@
+package esoterum.type;
 
+import mindustry.graphics.*;
+
+public class Led extends BinaryBlock {
+    public BinaryAcceptor(String name){
+        super(name);
+        rotate = true;
+    }
+
+    public class LedBuild extends BinaryBuild {
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
+            lastSignal = nextSignal;
+            nextSignal = signal();
+            signalOverride = false;
+            // sBack() | sLeft() | sRight()
+        }
+
+        @Override
+        public boolean signal(){
+            return false
+        }
+    }
+}
