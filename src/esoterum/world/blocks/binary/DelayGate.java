96f56be3ee1bf01f635584c8133a47896928ee9e
MEEP of Faith <meepmatthew@gmail.com>
2021-09-17 22:09:49 -0700

    Oversimplified Delay Gate Code

--- src/esoterum/world/blocks/binary/DelayGate.java
+++ src/esoterum/world/blocks/binary/DelayGate.java
@@ -12,30 +12,14 @@ public class DelayGate extends TimedBufferGate{
     }
 
     public class WaitGateBuild extends DelayGateBuild{
-        public boolean count;
-        public float outputTime, outputTimer;
-
         @Override
         public void updateTile(){
-            lastSignal = delayTimer >= delay();
-            nextSignal = signal();
-
-            if(signal()){
-                count = true;
-                outputTime = Mathf.clamp(outputTime + Time.delta, 0f, delay());
-            }
-
-            if(count && delayTimer < delay()){
-                delayTimer += Time.delta;
-            }else if(!signal()){
-                outputTimer += Time.delta;
-                if(outputTimer >= outputTime){
-                    delayTimer = 0;
-                    outputTimer = 0;
-                    outputTime = 0;
-                    count = false;
-                }
-            }
+            boolean s = signal();
+            nextSignal = s;
+            Time.run(delay(), () -> {
+                if(!isValid()) return;
+                lastSignal = s;
+            });
         }
 
         @Override
@@ -44,24 +28,8 @@ public class DelayGate extends TimedBufferGate{
             Draw.color(EsoVars.connectionOffColor, EsoVars.connectionColor, lastSignal ? 1f : 0f);
             Draw.rect(connectionRegion, x, y, rotdeg());
             Draw.rect(topRegion, x, y);
-            Draw.color(EsoVars.connectionOffColor, EsoVars.connectionColor, count ? 1f : 0f);
+            Draw.color(EsoVars.connectionOffColor, EsoVars.connectionColor, nextSignal ? 1f : 0f);
             Draw.rect(clock, x, y);
-        }
-
-        @Override
-        public void write(Writes write){
-            super.write(write);
-
-            write.f(outputTime);
-            write.f(outputTimer);
-        }
-
-        @Override
-        public void read(Reads read, byte revision){
-            super.read(read, revision);
-
-            outputTime = read.f();
-            outputTimer = read.f();
         }
     }
 }
